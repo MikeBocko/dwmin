16ad3cbf81f7189f51a7cbc95426056518e9248d
MikeBocko <Mike Bocko>
2025-06-14 14:50:20 +0000

    removed bloat

--- dwm.c
+++ dwm.c
@@ -20,20 +20,12 @@
  *
  * To understand everything else, start reading main().
  */
-#include <errno.h>
-#include <signal.h>
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <X11/cursorfont.h>
-#include <X11/keysym.h>
 #include <X11/Xatom.h>
-#include <X11/Xlib.h>
+
 #include <X11/Xproto.h>
-#include <X11/Xutil.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
 #endif /* XINERAMA */
